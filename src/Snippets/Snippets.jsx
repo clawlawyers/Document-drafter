c4d6223fc7c3650e1c46817d809661bbeabafed1
PushanAgrawal <billionear.pushan@gmail.com>
2024-08-14 12:00:33 +0530

    query box complete

--- src/Snippets/Snippets.jsx
+++ src/Snippets/Snippets.jsx
@@ -14,10 +14,56 @@ import "driver.js/dist/driver.css";
 import { draftTour } from "../utils/tour";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import QueryGIF from "../components/ui/QueryGIF";
+import axios from "axios";
+import giff from "../assets/icons/query.gif";
 const Snippets = () => {
   const doc_id = useSelector((state) => state.document.docId);
   console.log("doc id is", doc_id);
   const [showGIF, setShowGif] = useState(false);
+  const [query, setquery] = useState("");
+  const [textBoxData, setTextBoxData] = useState([]);
+  useEffect(() => {
+    var localnewData = JSON.parse(localStorage.getItem("newdata"));
+    if (localnewData !== null) {
+      setTextBoxData(localnewData);
+    }
+  }, []);
+  const hadnleSend = async () => {
+    let data = JSON.stringify({
+      doc_id: doc_id,
+      query: query,
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:8000/api/v1/ai-drafter/ask_question",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    var newdata = textBoxData;
+
+    newdata.push({
+      query: query,
+      response: {},
+      isLoading: false,
+    });
+    setTextBoxData([...newdata]);
+    console.log(textBoxData);
+    // setShowGif(true);
+
+    const response = await axios.request(config);
+    newdata[newdata.length - 1].isLoading = true;
+    newdata[newdata.length - 1].response = response;
+
+    setTextBoxData(newdata);
+    localStorage.setItem("newdata", JSON.stringify(newdata));
+    console.log(textBoxData);
+    console.log("query is", query);
+  };
 
   return (
     <div className="flex flex-row h-screen gap-3 p-6">
@@ -37,23 +83,34 @@ const Snippets = () => {
       </div>
       <div className="flex flex-col w-1/4 ">
         <NavbarLeft></NavbarLeft>
-        <div className="chat section flex relative flex-col  h-full  mt-4 p-2 gap-3 rounded-[0.625rem]   bg-customBlack">
-          {showGIF ? (
-            <TextBoxDialog></TextBoxDialog>
-          ) : (
-            <div className="flex flex-col h-screen justify-center items-center">
-              dads
+        <div className="chat section overflow-y-auto scrollbar-hide   flex relative flex-col   h-full  mt-4 p-2 gap-3 rounded-[0.625rem]   bg-customBlack">
+          {textBoxData.length > 0 ? (
+            <div className="flex flex-col pb-10 gap-3">
+              {textBoxData.map((item, i) => {
+                if (item.isLoading)
+                  return (
+                    <TextBoxDialog key={i} responseData={item}></TextBoxDialog>
+                  );
+                return <img key={i} src={giff}></img>;
+              })}
             </div>
+          ) : (
+            <QueryGIF />
           )}
-          <div className="absolute w-[95%] bottom-3">
+          <div className="sticky w-[95%] bottom-3">
             <input
               className="bg-white text-black rounded-md border-[0.05rem] border-black p-2 px-4 w-full"
               type="text"
               name=""
               placeholder="Enter Your Question..."
               id=""
+              value={query}
+              onChange={(e) => setquery(e.target.value)}
             />
-            <button className="absolute bottom-2 right-2 text-sm text-white bg-black p-1 px-3 rounded ">
+            <button
+              onClick={hadnleSend}
+              className="absolute bottom-2 right-2 text-sm text-white bg-black p-1 px-3 rounded "
+            >
               SEND
             </button>
           </div>
