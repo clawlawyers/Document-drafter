58da970195861e257b7c69118793d8d21241274c
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-17 14:24:31 +0530

    api bug fixed

--- src/actions/Summary.js
+++ src/actions/Summary.js
@@ -3,7 +3,7 @@ import { NODE_API_ENDPOINT } from "../utils/utils";
 
 export const getSummary = async (doc_id) => {
   try {
-    const res = await axios.post(`${NODE_API_ENDPOINT}ai-drafter/summarize`, {
+    const res = await axios.post(`${NODE_API_ENDPOINT}/ai-drafter/summarize`, {
       doc_id: doc_id,
     });
 
