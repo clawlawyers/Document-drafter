5479991f4d0f5d13988b3cdb9a733aaf525d2f2a
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-17 11:24:18 +0530

    NODEAPI added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 export const NODE_API_ENDPOINT =
   process.env.NODE_ENV === "production"
-    ? "https://claw-app.onrender.com/api/v1"
+    ? "https://claw-app-dev.onrender.com/api/v1"
     : "http://localhost:8000/api/v1";
