26c1c85a830498ce9a09f0966d86c2b6108232ef
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-08-22 18:07:32 +0530

    query spelling changed

--- src/components/ui/QueryGIF.jsx
+++ src/components/ui/QueryGIF.jsx
@@ -4,9 +4,9 @@ const QueryGIF = () => {
   return (
     <div className="flex flex-col gap-3 px-4  font-sans justify-center h-full items-center">
       <img className="h-[4.1rem]" src={gifs} alt="" />
-      <div className="font-bold text-lg">Have A Querry ?</div>
+      <div className="font-bold text-lg">Have A Query ?</div>
       <div className="text-sm text-center px-7">
-        Ask your querry and get relevant answers with respect to your document.
+        Ask your query and get relevant answers with respect to your document.
       </div>
     </div>
   );
