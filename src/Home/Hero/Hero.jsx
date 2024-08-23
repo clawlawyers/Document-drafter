03ecbdffe6d570ea6bb1100c614fa1e081abf90b
fuxsociety <musharafz2k3@gmail.com>
2024-08-23 12:25:05 +0530

    ui responsiveness issues

--- src/Home/Hero/Hero.jsx
+++ src/Home/Hero/Hero.jsx
@@ -14,38 +14,14 @@ const Hero = () => {
   //todo: get login credential like user._id to check if user is logged in or not and then setisLoggedIn to true
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex flex-col h-full   m-4  p-2 gap-3 rounded-[0.625rem]   bg-customBlack">
+    <div className="flex flex-col h-screen p-3">
+      <div className="flex flex-col h-full   p-2 gap-3 rounded-[0.625rem]   bg-customBlack">
+        <div className="h-[10%] w-full ">
+
         <HomeNav isLoggedIn={isLoggedIn} setLoginPopup={setLoginPopup} />
-        <div className="flex flex-col justify-between w-full h-full">
-          {/* <div className="flex flex-col gap-[6rem] items-center      ">
-            <div className="flex flex-col gap-4 items-center ">
-              <div className="font-sans font-medium text-xl ">Welcome to</div>
-              <div className="font-sans font-semibold text-6xl px-6   py-2 justify-items-center  bg-logo-gradient">
-                Adira AI
-              </div>
-              <div className="font-sans font-medium text-xl mt-2">
-                AI Powered Legal Document Drafter by CLAW
-              </div>
-            </div>
-            <div className="flex flex-row gap-7 cursor-pointer   ">
-              <div
-                onClick={() => navigate("/upload")}
-                className="hover:scale-110 duration-200 flex items-center flex-col gap-3 p-5 bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-14"
-              >
-                <img src={Cloud} alt="" />
-                <div>Upload Your Document</div>
-              </div>
-              <div onClick={()=> navigate("/Drafter")} className="flex hover:scale-110 duration-200 items-center flex-col gap-3 p-5 bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-14">
-                <img src={Prompt} alt="" />
-                <div>Create Document from Prompt</div>
-              </div>
-              <div onClick={()=> navigate("/DocType")} className="flex hover:scale-110 duration-200 items-center flex-col gap-3 p-5 bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-14">
-                <img src={Type} alt="" />
-                <div>Select Type of Document</div>
-              </div>
-            </div>
-          </div> */}
+        </div>
+        <div className="flex flex-col justify-between w-full h-[90%]">
+          
           <HeroPage></HeroPage>
 
           <Footer />
--- src/components/ui/HeroPage.jsx
+++ src/components/ui/HeroPage.jsx
@@ -8,7 +8,7 @@ import UserModal from "../../components/Modals/UserModal";
 const HeroPage = () => {
   let navigate = useNavigate();
   return (
-    <div className="flex flex-col gap-[6rem] items-center      ">
+    <div className="flex flex-col gap-5 items-center justify-center  h-[70%] ">
       <div className="flex flex-col gap-4 items-center ">
         <div className="font-sans font-medium text-xl ">Welcome to</div>
         <div className="font-sans font-semibold text-6xl px-6   py-2 justify-items-center  bg-logo-gradient">
@@ -18,27 +18,27 @@ const HeroPage = () => {
           AI Powered Legal Document Drafter by CLAW
         </div>
       </div>
-      <div className="flex flex-row gap-7 cursor-pointer   ">
+      <div className="flex flex-row gap-7 cursor-pointer scale-75  ">
         <div
           onClick={() => navigate("/upload")}
-          className="hover:scale-110 duration-200 flex items-center flex-col gap-3 p-5  bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-20"
+          className="hover:scale-110 duration-200 justify-center flex items-center flex-col gap-3 p-5  bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-20"
         >
           <img src={Cloud} alt="" />
-          <div>Upload Your Document</div>
+          <p className="text-center">Upload Your Document</p>
         </div>
         <div
           onClick={() => navigate("/Drafter")}
           className="flex hover:scale-110 duration-200 items-center flex-col gap-3 p-5 bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-14"
         >
           <img src={Prompt} alt="" />
-          <div>Create Document from Prompt</div>
+          <p className="text-center">Create Document from Prompt</p>
         </div>
         <div
           onClick={() => navigate("/DocType")}
           className="flex hover:scale-110 duration-200 items-center flex-col gap-3 p-5 bg-logo-gradient rounded-[0.9375rem] border-white border-[3px] px-20"
         >
           <img src={Type} alt="" />
-          <div>Select Type of Document</div>
+          <p className="text-center">Select Type of Document</p>
         </div>
       </div>
     </div>
